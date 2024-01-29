398c9442d1e7a814c2de28543e57841503e6b7eb
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-01-28 19:13:45 -0500

    feat: :sparkles: enable destructuring for exported classes in index.ts

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -1,6 +1,15 @@
-export * from './http-exception';
-export * from './bad-request';
-export * from './not-found';
-export * from './forbidden';
-export * from './unauthorized';
-export * from './internal-server-error';
+import * as HttpException from './http-exception';
+import * as BadRequestException from './bad-request';
+import * as NotFoundException from './not-found';
+import * as ForbiddenException from './forbidden';
+import * as UnauthorizedException from './unauthorized';
+import * as InternalServerErrorException from './internal-server-error';
+
+export {
+  HttpException,
+  BadRequestException,
+  NotFoundException,
+  ForbiddenException,
+  UnauthorizedException,
+  InternalServerErrorException
+};
