9c3d22bd80d8091b819edaa26879a2c556878f76
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-03-08 22:04:34 -0500

    refactor: :recycle: update imports and exports

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -1,23 +1,3 @@
-import HttpException from './http-exception';
-import BadRequestException from './4xx/400-bad-request';
-import UnauthorizedException from './4xx/401-unauthorized';
-import PaymentRequiredException from './4xx/402-payment-required';
-import ForbiddenException from './4xx/403-forbidden';
-import NotFoundException from './4xx/404-not-found';
-import MethodNotAllowedException from './4xx/405-method-not-allowed';
-import UnsupportedMediaTypeException from './4xx/415-unsupported-media-type';
-import InternalServerErrorException from './5xx/500-internal-server-error';
-import NotImplementedException from './5xx/501-not-implemented';
-
-export {
-  HttpException,
-  BadRequestException,
-  UnauthorizedException,
-  PaymentRequiredException,
-  ForbiddenException,
-  NotFoundException,
-  MethodNotAllowedException,
-  UnsupportedMediaTypeException,
-  InternalServerErrorException,
-  NotImplementedException
-};
+export * from './base-expection'
+export * from './4xx'
+export * from './5xx'
--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,16 @@
-import HttpException from './exceptions/http-exception';
-import BadRequestException from './exceptions/4xx/400-bad-request';
-import UnauthorizedException from './exceptions/4xx/401-unauthorized';
-import PaymentRequiredException from './exceptions/4xx/402-payment-required';
-import ForbiddenException from './exceptions/4xx/403-forbidden';
-import NotFoundException from './exceptions/4xx/404-not-found';
-import MethodNotAllowedException from './exceptions/4xx/405-method-not-allowed';
-import UnsupportedMediaTypeException from './exceptions/4xx/415-unsupported-media-type';
-import InternalServerErrorException from './exceptions/5xx/500-internal-server-error';
-import NotImplementedException from './exceptions/5xx/501-not-implemented';
+import {
+    HttpException,
+    BadRequestException,
+    UnauthorizedException,
+    PaymentRequiredException,
+    ForbiddenException,
+    NotFoundException,
+    MethodNotAllowedException,
+    UnsupportedMediaTypeException,
+    InternalServerErrorException,
+    NotImplementedException
+} from './exceptions';
+
 
 export type HttpExceptionType = typeof HttpException;
 export type BadRequestExceptionType = typeof BadRequestException;
