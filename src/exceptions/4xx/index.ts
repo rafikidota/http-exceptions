4699df0c7a97797e92a9dcfc812d5c95adf17398
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-12 13:15:31 -0400

    refactor: :recycle: exception exports

--- src/exceptions/4xx/index.ts
+++ src/exceptions/4xx/index.ts
@@ -1,17 +1,7 @@
-import BadRequestException from './400-bad-request';
-import UnauthorizedException from './401-unauthorized';
-import PaymentRequiredException from './402-payment-required';
-import ForbiddenException from './403-forbidden';
-import NotFoundException from './404-not-found';
-import MethodNotAllowedException from './405-method-not-allowed';
-import UnsupportedMediaTypeException from './415-unsupported-media-type';
-
-export {
-  BadRequestException,
-  UnauthorizedException,
-  PaymentRequiredException,
-  ForbiddenException,
-  NotFoundException,
-  MethodNotAllowedException,
-  UnsupportedMediaTypeException,
-};
+export * from './400-bad-request';
+export * from './401-unauthorized';
+export * from './402-payment-required';
+export * from './403-forbidden';
+export * from './404-not-found';
+export * from './405-method-not-allowed';
+export * from './415-unsupported-media-type';
--- src/exceptions/5xx/index.ts
+++ src/exceptions/5xx/index.ts
@@ -1,4 +1,5 @@
-import InternalServerErrorException from './500-internal-server-error';
-import NotImplementedException from './501-not-implemented';
-
-export { InternalServerErrorException, NotImplementedException };
+export * from './500-internal-server-error';
+export * from './501-not-implemented';
+export * from './502-bad-gateway';
+export * from './503-service-unavailable';
+export * from './504-gateway-timeout';
