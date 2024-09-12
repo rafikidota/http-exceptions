deece0126e7a09746b81489b76f372eb34a9ad22
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-12 13:18:32 -0400

    feat: :sparkles: add some server side exceptions exports

--- src/exceptions/5xx/index.ts
+++ src/exceptions/5xx/index.ts
@@ -1,4 +1,13 @@
 import InternalServerErrorException from './500-internal-server-error';
 import NotImplementedException from './501-not-implemented';
+import BadGatewayException from './502-bad-gateway';
+import ServiceUnavailableException from './503-service-unavailable';
+import GatewayTimeoutException from './504-gateway-timeout';
 
-export { InternalServerErrorException, NotImplementedException };
+export {
+  InternalServerErrorException,
+  NotImplementedException,
+  BadGatewayException,
+  ServiceUnavailableException,
+  GatewayTimeoutException,
+};
